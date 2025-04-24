8c013da570a37f0c2cafb40affe56f1edbe7b8cf
AishwaryaKPemmaiah <aishwaryakpemmaiah@gmail.com>
2025-04-24 18:04:56 +0530

    Removed unnecessary files and folders

--- retail_sales_ingestion_pipeline/scripts/transform_utils.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import DataFrame
-from pyspark.sql.functions import col
-
-def clean_sales_data(df: DataFrame) -> DataFrame:
-    return df.dropna().dropDuplicates()
-
-def cast_columns(df: DataFrame) -> DataFrame:
-    return df.withColumn("Quantity", col("Quantity").cast("integer")) \
-             .withColumn("Sales_Amount", col("Sales_Amount").cast("float"))
